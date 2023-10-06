0703c16b5ad5faa028e1848d0f0a565f241390d2
n <bimbo@prince.com>
2023-10-06 08:46:19 +0200

    fix: added comment to state.py

--- state.py
+++ state.py
@@ -11,6 +11,7 @@ class StateEntry:
 
 class State:
     """Holds the most important information about the state of the conversation with the guest"""
+
     def __init__(self):
         self.date_of_arrival = StateEntry('time', ['date', 'arrival'], 'Date of arrival', True)
         self.duration_of_stay = StateEntry('duration', ['duration'], 'Duration of stay', True)
@@ -18,6 +19,6 @@ class State:
         self.name_of_main_guest = StateEntry('text', ['name', 'guest'], 'The name of the main guest', True)
         self.email_address = StateEntry('email', ['email'], 'Email address', True)
         self.breakfast_included = StateEntry('bool', ['breakfast'], 'Does the guest want breakfast included?', True)
-        self.about_to_show_booking_summary = StateEntry('bool', ['booking summary', 'show'], '')
-        self.booking_confirmed = StateEntry('bool', ['confirm'], '')
-
+        self.about_to_show_booking_summary = StateEntry('bool', ['booking summary', 'show'],
+                                                        'Is a booking summary about to be sent?')
+        self.booking_confirmed = StateEntry('bool', ['confirm'], 'Did the user confirm the booking summary?')
